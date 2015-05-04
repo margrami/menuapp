a261a40cc2a3edee6a351321b3149c7f8432f23b
margrami <margrami221@yahoo.com>
2015-05-04 18:35:12 +0200

    adicionar linea en database read

--- Lesson_1/database_read.py
+++ Lesson_1/database_read.py
@@ -1,4 +1,4 @@
-# python script to read the database
+# [MRE] python script to read the database
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -6,14 +6,25 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from database_setup import Base, Restaurant, MenuItem
 
-
+# [MRE] the create_engine function lets our program know
+# which database engine we want to communicate
 engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.bind = engine
 
+#lets bind the engine to the Base class. This command
+#just makes the connections between our class definition
+# and the correspondig tables within database.
+Base.metadata.bind = engine
+#Create a sessionmaker object, this establishes a link
+#between our code executions and the engine 
 DBSession = sessionmaker(bind = engine)
+# in orden to CRUD on our database, sqlAlchemy use an 
+#interface named a session. this one instance of this
+#sesion. bat the things a not send to database until commit
 session = DBSession()
+
 
 items = session.query(MenuItem).all()
 for item in items:
 	print item.name
+	print "\n"
 
